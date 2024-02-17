af72feeec13bab0080b43e1f770c8a2e446f9bd0
Zach Girouard <zgirouard2@huskers.unl.edu>
2024-02-17 14:54:21 -0600

    WebScraper

--- IdentifyingArbitrage/Data/WebScraper.cs
+++ IdentifyingArbitrage/Data/WebScraper.cs
@@ -1,6 +1,29 @@
 namespace IdentifyingArbitrage.Data;
 
-public class WebScraper
+using HtmlAgilityPack;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml;
+using static System.Net.WebRequestMethods;
+
+public class Webscraper
 {
-    //do webscraping here
+    public Webscraper()
+    {
+        string source = "https://www.covers.com/sport/basketball/ncaab/odds";
+        HtmlWeb web = new HtmlWeb();
+        HtmlDocument document = web.Load(source);
+        IEnumerable<HtmlNode> nodes =
+            document.DocumentNode.Descendants(0)
+                .Where(n => n.HasClass("oddsGameRow"));
+        foreach (HtmlNode node in nodes)
+        {
+            Console.WriteLine(node);
+        }
+    }
 }
